49c2a25d31f2ccfb307d7fdbecab74284614a115
ProgrammerShahJalal <you@example.com>
2023-01-22 09:21:33 +0600

    order of N

--- complexity-theory.cpp
+++ complexity-theory.cpp
@@ -1,6 +1,31 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    
+int main()
+{
+
+    /* time and space complexity
+
+    ==> Judge time and space
+    ==>  */
+
+    int n, sum = 0; // O(1)
+    cin >> n;       // O(1)
+
+    // vector<int>a; O(1)
+    vector<int> a(n); // O(n)
+
+    for (int i = 0; i < n; i++) //O(n)
+    {
+        cin >> a[i];
+    }
+
+    for (int i = 0; i < n; i++) //O(n)
+    {
+        sum += a[i];
+    }
+
+    cout << sum << endl; //O(1)
+
+    // Total Time Complexity = O(1) + O(1) + O(n) + O(n) + O(n)+ O(1) = O(n)
     return 0;
 }
